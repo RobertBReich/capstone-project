5611f18125f7e63b927af5a7bdd42475e57af867
RobertBReich <mail@robertreich.de>
2022-09-07 18:20:39 +0200

    load content

--- src/pages/movies.js
+++ src/pages/movies.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import {useEffect, useState} from 'react';
 import styled from 'styled-components';
 
 import Layout from '../components/Layout';
@@ -15,9 +16,44 @@ const HaEins = styled.h1`
 
 export default function Movies() {
 	const API_KEY = process.env.API_KEY;
-	const GET_MOVIES = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 
-	const {loading, error, data} = useFetch(GET_MOVIES);
+	// let GET_MOVIES = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageCounter}`;
+
+	const [pageCounter, setPageCounter] = useState(1);
+	const [allData, setData] = useState([]);
+	const [nextURL, setNextURL] = useState('');
+	const [apiURL, setApiURL] = useState(
+		`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageCounter}`
+	);
+	const {loading, error, data} = useFetch(apiURL);
+
+	useEffect(() => {
+		console.log('currentData:');
+		console.log(allData);
+
+		let pageNextCounter = pageCounter + 1;
+
+		setNextURL(
+			`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNextCounter}`
+		);
+		if (data) {
+			setData(() => [...allData, ...data.results]);
+
+			console.log('data:');
+			console.log(data);
+		}
+	}, [data]);
+
+	function loadNextContent() {
+		console.log(nextURL);
+		setPageCounter(pageCounter + 1);
+		setApiURL(nextURL);
+	}
+	// function loadPreviousContent() {
+	// 	console.log(nextURL);
+	// 	setPageCounter(pageCounter - 1);
+	// 	setApiURL(previousURL);
+	// }
 
 	return (
 		<Layout>
@@ -32,7 +68,10 @@ export default function Movies() {
 			<HaEins>Movies</HaEins>
 			{loading && <p>Loading...</p>}
 			{error && <p>The content could not be loaded. Please try again.</p>}
+
 			{data && <MovieList data={data.results} />}
+			{/* <button onClick={loadPreviousContent}>load previous content</button> */}
+			<button onClick={loadNextContent}>load next content</button>
 		</Layout>
 	);
 }
